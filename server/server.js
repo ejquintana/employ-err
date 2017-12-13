e2910858786d00166da9a80dfcc64179d39c5e39
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-12 21:33:41 -0700

    initial site files

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const PORT = 8080;
+const app = express();
+
+app.get('/api', (req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
---
+++ src/components/Example/Example.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '/Example.css';
+
+
+
+
+
+function Example(props) {
+	return (
+		<div className="someClassNameFromExampleCss">
+			<h2>Some Shit Here...</h2>
+		</div>
+	)
+}
+
+
+
+
+
+export default Example;
