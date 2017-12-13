044f50d8f12ee1da23c79f2a6434b39a4603c572
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-13 16:42:37 -0700

    added home, employee, and employer api routes and created some fake data to play with

---
+++ server/employees.js
@@ -0,0 +1,9 @@
+const employees = [
+{id: 0, linkedInId: 2650987, displayName: "Fred Lintz", title: "Full Stack Web Ninja", bio: "Super chill, superfly, super coder.", email: "fred@email.com", skills: ['html5', 'css', 'javascript', 'react', 'mongo'], searchable: true, matches: [], connections: [], denied: []},
+{id: 1, linkedInId: 92830987, displayName: "Ed Quintana", title: "Full Stack Web Sensai", bio: "I eat coders like you for breakfast", email: "ed@email.com", skills: ['html5', 'css', 'javascript', 'react', 'mongo'], searchable: true, matches: [], connections: [], denied: []},
+{id: 2, linkedInId: 8375987,  displayName: "Ash Rotman", title: "Full Stack Web Yoda", bio: "#makingWebsites #crushingCoffees", email: "ash@email.com", skills: ['html5', 'css', 'javascript', 'react', 'mongo'], searchable: true, matches: [], connections: [], denied: []},
+{id: 3, linkedInId: 09843597,  displayName: "Megan Landahl", title: "Full Stack Web Czar", bio: "I bleed in binary", email: "megan@email.com", skills: ['html5', 'css', 'javascript', 'react', 'mongo'], searchable: true, matches: [], connections: [], denied: []}
+]
+
+
+module.exports = employees;
---
+++ server/employers.js
@@ -0,0 +1,7 @@
+const employers = [
+{id: 0, linkedInId: 982365908, displayName: "Mark Zuckerberg", title: "Ceo - Facebook", bio: "Facebook knows everything about you", email: "facebook@email.com", webiste: "facebook.com", matches: [], connections: [], denied: []},
+{id: 1, linkedInId: 287623876, displayName: "Elon Musk", title: "Ceo - SpaceX", bio: "Tesla Cars are so badass", email: "elon@email.com", website: "tesla.com", matches: [], connections: [], denied: []}
+]
+
+
+module.exports = employers;
