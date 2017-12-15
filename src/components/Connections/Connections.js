91a9f3960f7695f96de345de9b980f552e8c1131
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-15 16:53:36 -0700

    added card.js, and styledmatches, and connections to be responsive with multiple cards inside

--- src/components/Connections/Connections.js
+++ src/components/Connections/Connections.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../Card/Card';
+
 
 
 function Connections() {
@@ -7,8 +9,13 @@ function Connections() {
 	  <div className="card-header" style={{padding: "5px 0px 0px 10px"}}>
 	  	<h4>Connections</h4>
 	  </div>
-	  <div className="card-block" style={{height: "200px", overflow: 'auto', padding: "10px"}}>
-	  	<p>Insert Connection Cards here...</p>
+	<div className="card-block container" style={{height: "500px", overflow: "auto", padding: "10px"}}>
+		<div className="row">
+		  	<Card displayName="Ron Swanson" title="Full Stack Web Ninja"/>
+		  	<Card displayName="Ron Swanson" title="Full Stack Web Ninja"/>
+		  	<Card displayName="Ron Swanson" title="Full Stack Web Ninja"/>
+		  	<Card displayName="Ron Swanson" title="Full Stack Web Ninja"/>
+		</div>
 	  </div>
 	</div>
 	)
