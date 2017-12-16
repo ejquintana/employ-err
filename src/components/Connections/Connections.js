c9a5903ac599e6584aeedef870ba3647885978c1
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-16 14:20:08 -0700

    changed card styling completely

--- src/components/Connections/Connections.js
+++ src/components/Connections/Connections.js
@@ -6,22 +6,21 @@ import Card from '../Card/Card';
 class Connections extends Component {
 	render() {
 		return (
-			<div className="card" style={{width: "100%"}}>
+			<div className="card" style={{width: "100%", height: "70vh"}}>
 			  <div className="card-header" style={{padding: "5px 0px 0px 10px"}}>
 			  	<h4>Connections</h4>
 			  </div>
-			<div className="card-block container" style={{height: "500px", overflow: "auto", padding: "10px"}}>
-				<div className="row">
-				  	{this.props.data.map((card, index) => (
-				  		<Card 	
-				  			key={index} 
-				  			displayName={card.displayName} 
-				  			image={card.image}
-				  			title={card.title}
-				  			email={card.email}
-				  			toggle={this.props.toggle}
-				  			href={this.props.href}/>))}
-				</div>
+			<div className="card-block container" style={{height: "500px", overflow: "auto"}}>
+			  	{this.props.data.map((card, index) => (
+			  		<Card 	
+			  			key={index} 
+			  			displayName={card.displayName} 
+			  			image={card.image}
+			  			title={card.title}
+			  			bio={card.bio}
+			  			email={card.email}
+			  			toggle={this.props.toggle}
+			  			href={this.props.href}/>))}
 			  </div>
 			</div>
 		)
--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -7,21 +7,20 @@ class Matches extends Component {
 
 	render() {
 		return (
-			<div className="card" style={{width: "100%"}}>
+			<div className="card" style={{width: "100%", height: "70vh"}}>
 			  <div className="card-header" style={{padding: "5px 0px 0px 10px"}}>
 		  		<h4>Pending Matches...</h4>
 			  </div>
-			  <div className="card-block container" style={{height: "500px", overflow: "auto", padding: "10px"}}>
-			  	<div className="row">
-				  	{this.props.data.map((card, index) => (
-				  		<Card 	
-				  			key={index} 
-				  			displayName={card.displayName} 
-				  			image={card.image}
-				  			title={card.title}
-				  			href={this.props.href}
-				  			toggle={this.props.toggle}/>))}
-				</div>
+			  <div className="card-block container" style={{height: "500px", overflow: "auto"}}>
+			  	{this.props.data.map((card, index) => (
+			  		<Card 	
+			  			key={index} 
+			  			displayName={card.displayName} 
+			  			image={card.image}
+			  			title={card.title}
+			  			bio={card.bio}
+			  			href={this.props.href}
+			  			toggle={this.props.toggle}/>))}
 			  </div>
 			</div>
 		)
