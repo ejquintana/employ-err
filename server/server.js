608929cddbc86c173e1b42ef6c1bc35415a15e1c
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-16 20:26:01 -0700

    debugging heroku

--- server/server.js
+++ server/server.js
@@ -17,8 +17,12 @@ app.get('/api/employers', (req, res) => {
 	res.send(employers);
 });
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname,'../public/index.html'));
+// app.get('/*', (req, res) => {
+// 	res.sendFile(path.join(__dirname,'../public/index.html'));
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 app.listen(PORT, () => console.log(`Employ{err} app server listening on port ${PORT}`));
