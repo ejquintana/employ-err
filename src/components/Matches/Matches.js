8ab018d309d6c28ac3ea8d218e893735f51b9960
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-12-16 21:06:11 -0700

    added empty array or this.props

--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -5,14 +5,19 @@ import Card from '../Card/Card';
 
 class Matches extends Component {
 
+
 	render() {
+
+		let array = this.props.data || [];
+
 		return (
 			<div className="card" style={{width: "100%", height: "70vh"}}>
 			  <div className="card-header" style={{padding: "5px 0px 0px 10px"}}>
 		  		<h4>Pending Matches...</h4>
 			  </div>
 			  <div className="card-block container" style={{height: "500px", overflow: "auto"}}>
-			  	{this.props.data.map((card, index) => (
+			  	{}
+			  	{array.map((card, index) => (
 			  		<Card 	
 			  			key={index} 
 			  			displayName={card.displayName} 
